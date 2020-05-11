e63948b282ec2c18e6cba38ac03b02a2dc31ff0e
Jits <dev@jits.co.uk>
2020-05-11 13:56:14 +0100

    Use 'eva-icons' instead material icons

--- yana-frontend/quasar.conf.js
+++ yana-frontend/quasar.conf.js
@@ -30,13 +30,13 @@ module.exports = configure(function (ctx) {
       // 'ionicons-v4',
       // 'mdi-v5',
       // 'fontawesome-v5',
-      // 'eva-icons',
+      'eva-icons',
       // 'themify',
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      // 'material-icons', // optional, you are not bound to it
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
