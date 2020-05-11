1e3d80a408183003236e14a1c30efe23adf75634
Jits <dev@jits.co.uk>
2020-05-11 13:59:42 +0100

    Change the `vueRouterMode` to `history`

--- yana-frontend/quasar.conf.js
+++ yana-frontend/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // rtl: false, // https://quasar.dev/options/rtl-support
       // preloadChunks: true,
