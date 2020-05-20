8e22b195e7da47f6d5c2d0107c86c43df53949c6
Jits <dev@jits.co.uk>
2020-05-20 15:51:20 +0100

    [backend] Set up a basic logger for now

--- yana-backend/src/main.ts
+++ yana-backend/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // see https://expressjs.com/en/guide/behind-proxies.html
   app.set('trust proxy', 1);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
