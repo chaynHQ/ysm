6adf9444cd57566d93fbf2c12068f63a3e79f04b
Jits <dev@jits.co.uk>
2020-05-20 15:39:41 +0100

    [backend] Trust the upstream proxy
    This is so we can get the original IP for requests to the backend.
    
    Note: this assumes that we will only ever serve the backend service behind a proxy.

--- yana-backend/src/main.ts
+++ yana-backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // see https://expressjs.com/en/guide/behind-proxies.html
+  app.set('trust proxy', 1);
   await app.listen(3000);
 }
 bootstrap();
