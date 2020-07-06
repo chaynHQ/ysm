5f71e459e17cad0c76ed7b64baa0056fc5e4f1ff
Jits <dev@jits.co.uk>
2020-07-06 14:11:33 +0100

    [backend] Specify the Config interface explicitly (to fix linting)

--- ysm-backend/src/config.ts
+++ ysm-backend/src/config.ts
@@ -1,8 +1,15 @@
-function missing(key: string): void {
+function missing(key: string): string {
   throw new Error(`Missing env var: ${key}`);
 }
 
-export default () => ({
+export interface Config {
+  port: string | number;
+  storyblok: {
+    token: string;
+  };
+}
+
+export default (): Config => ({
   port: process.env.PORT || 3000,
   storyblok: {
     token: process.env.STORYBLOK_TOKEN || missing('STORYBLOK_TOKEN'),
