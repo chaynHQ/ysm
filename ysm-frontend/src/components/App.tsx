41597a112dd5a66366dbab4b2bd84f11bdb6fc7e
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-07-13 15:43:05 +0100

    Home page

--- ysm-frontend/src/components/App.tsx
+++ ysm-frontend/src/components/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from '../styles/app.module.css';
-import {Container, Box, Grid} from '@material-ui/core';
+import {Container, Box, makeStyles} from '@material-ui/core';
+
 import Page from './Page'
 
 import {isMobile} from 'react-device-detect';
 
+const useStyles = makeStyles({
+  appContainer: {
+    height: '100vh',
+    backgroundColor: '#FFF5F0',
+  },
+  
+  pageContainerDesktop : {
+    height:731,
+    width:752,
+  },
+  
+  pageContainerMobile:  {
+    height:'100vh',
+    width:'100vw',
+  },
+
+  container: {
+    padding: 0,
+  }
+});
 
 
-const App = () => (
-  <Box 
-    className={styles.appContainer} 
+
+
+function App() {
+  const classes = useStyles();
+
+  return (  <Box 
+    className={classes.appContainer} 
     display="flex" 
     alignItems="center"
     justifyContent="center">
 
       <Box boxShadow={3}>
-        <Container maxWidth="sm">
+        <Container className={`${classes.container} ${isMobile ? classes.pageContainerMobile : classes.pageContainerDesktop}`}>
           <Page />
         </Container>
       </Box>
-  </Box>
-);
+  </Box>)
+};
 
 export default App;
--- ysm-frontend/src/components/Page.tsx
+++ ysm-frontend/src/components/Page.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import styles from '../styles/page.module.css';
-import {Container, Button, makeStyles} from '@material-ui/core';
+import {Container, Button, makeStyles, Grid, Typography} from '@material-ui/core';
 import logo from '../assets/logo.png';
+import backgroundImage from '../assets/background.png'
+
+const useStyles = makeStyles({
+    logo: {
+      height: 48,
+    },
+    container: {
+      height: '100%',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }
+  });
+  
 
 // TODO: Set background
 // TODO: Set styles
-const Page = () => (
-    <Container>
-        <img src={logo} alt={'Your story matters logo'} />
-        <p>If you’ve been treated in a way that has made you feel uncomfortable or violated, use YSM to find out how to get support.</p>
-        <p>Let YSM be your companion because You Are Not Alone.</p>
-        <Button>START YOUR JOURNEY</Button>
+function  Page(){
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Grid container justify="center" direction="column" alignItems="center">
+        <img 
+            src={logo}
+            alt={'Your story matters logo'}
+            className={classes.logo} 
+        />
+        <Typography>If you’ve been treated in a way that has made you feel uncomfortable or violated, use YSM to find out how to get support.</Typography>
+        <Typography>Let YSM be your companion because You Are Not Alone.</Typography>
+        <Button variant="contained" color="primary">START YOUR JOURNEY</Button>
+      </Grid>
     </Container>
-);
+  )
+};
 
 export default Page;
