eaa7fdc02b2b4249d0238f265a7c49a7490ac788
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-08-03 17:58:08 +0100

    Linting & testing

--- ysm-frontend/src/tests/App.test.tsx
+++ ysm-frontend/src/tests/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../components/App';
+import '@testing-library/jest-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = getByText('START');
+  expect(el).toBeInTheDocument();
 });
