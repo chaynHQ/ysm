730dec66c90217e88e5cb94b1b4adb534fa9d707
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-08-05 16:58:22 +0100

    password, email & name update

--- ysm-frontend/src/components/Settings.jsx
+++ ysm-frontend/src/components/Settings.jsx
@@ -5,28 +5,115 @@ import firebase from '../config/firebase';
 
 import SignIn from './SignIn';
 
+import { setSettingsAuth } from '../store/actions';
+
 
 const Settings = (props) => {
 
-    const [name, setName] = useState(props.user.displayName)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+   const resetPassword = (newPassword) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            user.updatePassword(newPassword).then(function() {
+                // Update successful.
+            }).catch(function(error) {
+                if(error.code === 'auth/weak-password'){
+                    //Show some sort of weak password message
+                } else if( error.code === 'auth/requires-recent-login'){
+                    props.setSettingsAuth(false)
+                }
+                else {
+                    throw error
+                }
+            });
+        } else {
+          // No user is signed in.
+        }
+      }); 
+   }
+
+   const resetEmail = (newEmail) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            user.updateEmail(newEmail).then(function() {
+                // Update successful.
+            }).catch(function(error) {
+                if( error.code === 'auth/requires-recent-login'){
+                    props.setSettingsAuth(false)
+                }
+                else {
+                    throw error
+                }
+            });
+        } else {
+          // No user is signed in.
+        }
+      }); 
+   }
+
+   const updateName = (newName) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            user.updateProfile({
+                displayName: newName,
+              }).then(function() {
+                // Update successful.
+            }).catch(function(error) {
+                    throw error
+            });
+        } else {
+          // No user is signed in.
+        }
+      }); 
+   }
 
 
   return (
     <Box display="flex" flexDirection="column" height={1}>
         <Typography>Settings</Typography>
         { props.settingsAuth ? 
+        <Box>
             <Box>
-            <TextField 
-                id="standard-basic" 
-                placeholder={name} 
-                helperText="Display Name" 
-                onChange={(event)=>{
-                    setName(event.target.value)
-                }} 
-            />
-            <Button onClick={() => { alert('clicked') }}>
-                Update
-            </Button>
+                <TextField 
+                    id="standard-basic" 
+                    placeholder={props.user.displayName} 
+                    helperText="Display Name" 
+                    onChange={(event)=>{
+                        setName(event.target.value)
+                    }} 
+                />
+                <Button onClick={() => {updateName(name) }}>
+                    Update
+                </Button>
+            </Box>
+            <Box>
+                <TextField 
+                    id="standard-basic" 
+                    placeholder={props.user.email} 
+                    helperText="Email" 
+                    onChange={(event)=>{
+                        setEmail(event.target.value)
+                    }} 
+                />
+                <Button onClick={() => { resetEmail(email) }}>
+                    Update
+                </Button>
+            </Box>
+            <Box>
+                <TextField 
+                    id="standard-basic"  
+                    helperText="Password" 
+                    onChange={(event)=>{
+                        setPassword(event.target.value)
+                    }} 
+                />
+                <Button onClick={() => { resetPassword(password) }}>
+                    Update
+                </Button>
+            </Box>
         </Box>
         :
         <SignIn redirectUrl = '/settings' />
@@ -42,6 +129,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+    setSettingsAuth: (bool) => dispatch(setSettingsAuth(bool)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings);
