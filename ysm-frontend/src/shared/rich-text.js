2dc58d948718d3ba6d9ee9ef9c37d740d906d04a
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-08-17 14:48:27 +0100

    Remove testing log

--- ysm-frontend/src/shared/rich-text.js
+++ ysm-frontend/src/shared/rich-text.js
@@ -3,6 +3,5 @@ import RichTextResolver from 'storyblok-js-client/dist/richTextResolver';
 const richTextResolver = new RichTextResolver();
 
 export default function richTextHelper(content) {
-  console.log(content);
   return richTextResolver.render(content);
 }
