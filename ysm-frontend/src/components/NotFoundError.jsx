9cda34fe04ae215719a46a5b309e1dbeac65ece4
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-08-19 15:09:53 +0100

    Add theme content

---
+++ ysm-frontend/src/components/NotFoundError.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Typography, Box } from '@material-ui/core';
+
+const NotFoundError = () => (
+  <Box>
+    <Typography>404</Typography>
+  </Box>
+
+);
+
+export default NotFoundError;
