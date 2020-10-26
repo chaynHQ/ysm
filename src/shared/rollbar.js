40c4bcb7eb846112df2fa449095ebf75441440c8
SophieHeb <sophie.hebdidge@gmail.com>
2020-10-26 15:17:48 +0000

    Token checks

--- src/shared/rollbar.js
+++ src/shared/rollbar.js
@@ -1,7 +1,7 @@
 import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
-  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN,
+  accessToken: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN : null,
   captureUncaught: true,
   captureUnhandledRejections: true,
   enabled: (process.env.NODE_ENV === 'production'),
