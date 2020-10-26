b9d170c2302d8a129875ac30080aa0c4ecaaeb4e
SophieHeb <sophie.hebdidge@gmail.com>
2020-10-26 17:24:59 +0000

    Add in Styled Error pages

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,14 +1,48 @@
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import LinkUi from '@material-ui/core/Link';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Rollbar from 'rollbar';
 
+const useStyles = makeStyles({
+  image: {
+    width: '85%',
+  },
+});
+
 function Error({ statusCode }) {
+  const classes = useStyles();
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server!`
-        : 'An error occurred on client'}
-    </p>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height={1}
+    >
+      <Typography variant="h1">
+        Error
+        {' '}
+        {statusCode}
+      </Typography>
+      <img
+        className={classes.image}
+        src="/error-illustration.png"
+        alt="Line drawing of two people sat down having a conversation."
+      />
+      <Typography align="center">Oh no!</Typography>
+      <Typography align="center">Looks like there is a mistake!</Typography>
+      <Typography align="center">
+        This has been reported to us and we are working on it. Head back to the
+        {' '}
+        <Link href="/">
+          <LinkUi color="inherit" underline="always">homepage</LinkUi>
+        </Link>
+        {' '}
+        for now.
+      </Typography>
+    </Box>
   );
 }
 
