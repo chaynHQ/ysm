488983948e252b79db47a1dc27ea2e8d39f30629
sophieheb <sophie.hebdidge@gmail.com>
2020-11-23 15:10:48 +0000

    Replace process.env with runtime config (#107)
    * Replace process.env with runtime config
    
    * Mock config for tests
    
    * Move definition of publicRuntimeConfig
    
    * Add in env for static pages
    
    * Switch rollbar token around
    
    * Switch token around
    
    * Update CI
    
    * Workflow file fix

--- src/config/mailchimp.js
+++ src/config/mailchimp.js
@@ -1,8 +1,11 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 mailchimp.setConfig({
-  apiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
-  server: process.env.NEXT_PUBLIC_MAILCHIMP_SERVER_PREFIX,
+  apiKey: publicRuntimeConfig.NEXT_PUBLIC_MAILCHIMP_API_KEY,
+  server: publicRuntimeConfig.NEXT_PUBLIC_MAILCHIMP_SERVER_PREFIX,
 });
 
 export default mailchimp;
