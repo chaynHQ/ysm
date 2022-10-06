57a95b252b7e5c1a1735c22150ea90b3ad33c4ea
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-10-06 09:52:36 +0100

    Upgrade firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
-import 'firebase/analytics';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import 'firebase/compat/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import getConfig from 'next/config';
 import { Cookies } from 'react-cookie-consent';
 import isBrowser from '../shared/browserCheck';
