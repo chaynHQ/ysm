855819b278b65e3fcd6d25347eda22be2429a641
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-05-28 20:11:11 +0100

    Remove unused import

--- src/config/mailchimp.js
+++ src/config/mailchimp.js
@@ -1,7 +1,5 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
-import getConfig from 'next/config';
 
-const { serverRuntimeConfig } = getConfig();
 mailchimp.setConfig({
   apiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
   server: process.env.NEXT_PUBLIC_MAILCHIMP_SERVER_PREFIX,
