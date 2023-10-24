36065b5abb126bd2d8d8f93384701e392a57c845
annarhughes <annaraehughes@live.co.uk>
2023-10-24 22:54:36 +0100

    fix live NEXT_PUBLIC_ENV

--- src/shared/analyticsEvent.js
+++ src/shared/analyticsEvent.js
@@ -1,7 +1,7 @@
 import firebase from '../config/firebase';
 
 export default function analyticsEvent(event, params) {
-  if (process.env.NEXT_PUBLIC_ENV === 'production') {
+  if (process.env.NEXT_PUBLIC_ENV === 'live') {
     firebase.analytics().logEvent(event, params);
   }
 }
