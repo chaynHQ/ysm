cc915aaad1c285e58e94c53838203efb56562f4c
Anna Hughes <annaraehughes@live.co.uk>
2023-11-06 12:06:28 +0000

    fix hotjar NPS stylesheet import preload (#220)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Nunito&display=swap"
           />
-          <link href="https://bloom.chayn.co/hotjarNPS.css" rel="stylesheet" type="text/css" />
+          <link href="https://bloom.chayn.co/hotjarNPS.css" rel="preload" as="style" />
         </Head>
         <body>
           <Main />
