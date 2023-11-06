ec6fd48d6dd473af54dc1014db9e8ee13a9ed61b
Anna Hughes <annaraehughes@live.co.uk>
2023-11-06 14:54:54 +0000

    Add internal hotjarNPS.css (#222)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Nunito&display=swap"
           />
-          <link rel="stylesheet" href="https://bloom.chayn.co/hotjarNPS.css" />
         </Head>
         <body>
           <Main />
